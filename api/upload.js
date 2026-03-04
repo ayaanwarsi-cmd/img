372b8cd179f8a9995afe4843aad2bb4543cb7564
ayaanwarsi-cmd <ayaanwarsi816@gmail.com>
2026-03-04 18:34:17 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,17 +1,25 @@
 export default async function handler(req,res){
 
-const token=process.env.GITHUB_TOKEN
-const username="ayaanwarsi-cmd"
-const repo="img"
-const folder="images"
+if(req.method !== "POST"){
+return res.status(405).json({error:"Method not allowed"})
+}
 
-const {name,data}=req.body
+const token = process.env.GITHUB_TOKEN
+const username = "ayaanwarsi-cmd"
+const repo = "img"
+const folder = "images"
 
-const filename=Date.now()+"-"+name
+const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body
 
-let url=`https://api.github.com/repos/${username}/${repo}/contents/${folder}/${filename}`
+const name = body.name
+const data = body.data
 
-await fetch(url,{
+const filename = Date.now()+"-"+name
+
+const githubURL =
+`https://api.github.com/repos/${username}/${repo}/contents/${folder}/${filename}`
+
+const response = await fetch(githubURL,{
 method:"PUT",
 headers:{
 Authorization:`token ${token}`,
@@ -23,7 +31,19 @@ content:data
 })
 })
 
-let imageURL=`https://raw.githubusercontent.com/${username}/${repo}/main/${folder}/${filename}`
+if(!response.ok){
+
+const err = await response.text()
+
+return res.status(500).json({
+error:"GitHub upload failed",
+details:err
+})
+
+}
+
+const imageURL =
+`https://raw.githubusercontent.com/${username}/${repo}/main/${folder}/${filename}`
 
 res.json({url:imageURL})
 
