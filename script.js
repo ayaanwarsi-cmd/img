61cd903d2c2be14ed9d79be6bbb3b282b478cd75
ayaanwarsi-cmd <ayaanwarsi816@gmail.com>
2026-03-04 17:35:19 +0530

    Refactor image upload and loading functions

--- script.js
+++ script.js
@@ -1,80 +1,37 @@
-const username = "YOUR_GITHUB_USERNAME"
-const repo = "YOUR_REPO_NAME"
-const folder = "images"
-const token = "YOUR_GITHUB_TOKEN"
+const gallery=document.getElementById("gallery")
 
-const gallery = document.getElementById("gallery")
+document.getElementById("upload").addEventListener("change",uploadImage)
 
-async function loadImages(){
+async function uploadImage(e){
 
-gallery.innerHTML="Loading..."
+let file=e.target.files[0]
 
-let url=`https://api.github.com/repos/${username}/${repo}/contents/${folder}`
+if(!file) return
 
-let res = await fetch(url)
-let data = await res.json()
+let formData=new FormData()
+formData.append("file",file)
 
-gallery.innerHTML=""
+let res=await fetch("/api/upload",{
+method:"POST",
+body:formData
+})
 
-data.forEach(file=>{
+let data=await res.json()
 
-if(file.type==="file"){
+addImage(data.url)
+
+}
+
+function addImage(url){
 
 let div=document.createElement("div")
-div.className="image-card"
+div.className="card"
 
 div.innerHTML=`
-<img src="${file.download_url}">
-<p>${file.name}</p>
-<button onclick="renameImage('${file.path}')">Rename</button>
+<img src="${url}">
+<input value="${url}" onclick="this.select()">
 `
 
-gallery.appendChild(div)
-
-}
-
-})
-
-}
-
-loadImages()
-
-document.getElementById("upload").addEventListener("change",async function(){
-
-let file=this.files[0]
-
-let reader=new FileReader()
-
-reader.onload=async function(){
-
-let content=reader.result.split(",")[1]
-
-let url=`https://api.github.com/repos/${username}/${repo}/contents/${folder}/${file.name}`
-
-await fetch(url,{
-method:"PUT",
-headers:{
-Authorization:`token ${token}`
-},
-body:JSON.stringify({
-message:"upload image",
-content:content
-})
-})
-
-loadImages()
-
-}
-
-reader.readAsDataURL(file)
-
-})
-async function renameImage(path){
-
-let newName=prompt("New name")
-
-if(!newName)return
-
-alert("Rename in GitHub requires delete + upload again. Implement later.")
+gallery.prepend(div)
 
 }
