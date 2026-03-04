cb4e8c9559a8371aca0d6274193d83db62885fdd
ayaanwarsi-cmd <ayaanwarsi816@gmail.com>
2026-03-04 17:58:54 +0530

    Add delete API handler for GitHub files
    Implement API endpoint to delete files from GitHub repository.

---
+++ api/delete.js
@@ -0,0 +1,29 @@
+export default async function handler(req,res){
+
+const token=process.env.GITHUB_TOKEN
+
+const username="ayaanwarsi-cmd"
+const repo="img"
+
+const {url}=req.body
+
+const path=url.split("/main/")[1]
+
+let file=await fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`)
+let data=await file.json()
+
+await fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`,{
+method:"DELETE",
+headers:{
+Authorization:`token ${token}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+message:"delete image",
+sha:data.sha
+})
+})
+
+res.json({success:true})
+
+}
